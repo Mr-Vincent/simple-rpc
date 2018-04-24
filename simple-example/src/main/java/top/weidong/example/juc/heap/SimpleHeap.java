c0c20886ccabbd223cd0b5cd3d7f615a52be9a7d
dongwei <dong.wei.love@163.com>
2018-04-24 09:52:50 +0800

    堆简单 bugfix 下标为0的元素参与堆化

--- simple-example/src/main/java/top/weidong/example/juc/heap/SimpleHeap.java
+++ simple-example/src/main/java/top/weidong/example/juc/heap/SimpleHeap.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
  * Description:
  * 堆的简单实现(小堆 小的在前 大的靠后)
  *
+ *
  * @author dongwei
  * @date 2018/04/23
  * Time: 16:36
@@ -22,7 +23,7 @@ public class SimpleHeap {
 
     public SimpleHeap(int[] data) {
         this.data = data;
-        this.size = data.length-1;
+        this.size = data.length;
         heapify();
     }
 
@@ -30,8 +31,8 @@ public class SimpleHeap {
      * 堆化
      */
     private void heapify() {
-        //下标小于等于i的节点拥有子节点
-        for (int i = size / 2;  i >= 1; i--) {
+        // 针对有子节点的元素进行处理
+        for (int i = size / 2 - 1;  i >= 0; i--) {
             fixDown(i);
         }
     }
@@ -42,8 +43,8 @@ public class SimpleHeap {
      */
     private void fixDown(int k) {
         int j;
-        while ((j = k << 1) <= size && j > 0) {
-            if (j < size &&
+        while ((j = (k << 1) + 1) <= size && j > 0) {
+            if (j < size-1 &&
                     data[j] > data[j+1]) {
                 // j indexes smallest kid
                 j++;
@@ -57,8 +58,7 @@ public class SimpleHeap {
     }
 
     public static void main(String[] args) {
-        // 问题是第一个元素没有参与 原因是当使用数组表示二叉树时，某一个节点的孩子节点为当前下标的2n和2n+1 当第0个元素为根节点没办法计算子节点 只能从下标1开始
-        int[] a = {10,8,2,3,9};
+        int[] a = {5,4,3,2,1,0};
         SimpleHeap heap = new SimpleHeap(a);
         System.out.println(Arrays.toString(a));
     }
